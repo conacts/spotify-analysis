4fae53d5a68ab26c7afbb656d1c0467c4c686a91
cs <csheehan630@gmail.com>
2022-10-13 21:22:19 -0400

    trying a fix

--- authorization_code/public/getData.js
+++ authorization_code/public/getData.js
@@ -5,7 +5,10 @@ function buildArtists50(r) {
     $('#artists').append('<ul class="list-group list-group-flush" style="margin-top: 4rem;"');
     var c = "list-group-item";
     for (let i=0; i < data.length; i++) {
-        $('#artists').append('<li class="' + c + '">' + data[i].name + '</li>')
+        var artist_img = '<a><img width="64" height="64" src="' + data[i].images[2].url + '"></a>';
+        var artist_name = '<a style="font-style: bold; font-size: 1.5rem; margin-left: 2rem;">' + (i+1) + '. ' + data[i].name + '</a>';
+        $('#artists').append('<li class="' + c + '">' + artist_img + artist_name + '</li>')
+
     }
     $('#artists').append('</ul>');
 }
@@ -17,9 +20,13 @@ function buildTracks50(r) {
     $('#songs').append('<ul class="list-group list-group-flush" style="margin-top: 4rem;"');
     var c = "list-group-item";
     for (let i=0; i < data.length; i++) {
-        var vid = '<video controls="" name="media"><source src="' + data[i].preview_url + '" type="audio/mpeg"></video>';
-        var album_cover = '<img src="' + data[i].album.images[1].url + '">';
-        $('#songs').append('<li class="' + c + '"><a>' + (i+1) + '. ' + data[i].name + '</a><a>' + vid + '</a>' + album_cover + '</li>')
+        var vid = '<video style="text-align: center;" controls="" name="media"><source src="' + data[i].preview_url + '" type="audio/mpeg"></video>';
+        var track_name = '<a style="font-style: bold; font-size: 1.5rem; margin-left: 2rem;">' + (i+1) + '. ' + data[i].name + '</a>';
+        if (data[i].preview_url == undefined) {
+            vid = '';
+        }
+        var album_cover = '<a><img src="' + data[i].album.images[2].url + '"></a>';
+        $('#songs').append('<li class="' + c + '">'  + album_cover + track_name + '</li>')
     }
     $('#songs').append('</ul>');
 }
